2d6d58d873c40165179edc503ea261c250604004
huang puguang <puguang_huang@fpi-inc.com>
2019-05-08 21:07:32 +0800

    feat: 提示高亮

--- src/router/router.js
+++ src/router/router.js
@@ -3,53 +3,53 @@ import { execAsync } from '../exec/exec'
 import config from '../config'
 import path from 'path'
 import crypto from 'crypto'
+import { log } from '../utils'
 
 let router = express.Router()
 
 config.gitRepos.map(repo => {
     router.post(`/${repo.router}`, async (req, res) => {
-		// secret校验
-		console.log('[auto] 收到请求，正在校验签名')
+        // secret校验
+        log('收到请求，正在校验签名')
 		const secret = req.headers['x-hub-signature']
 		if (secret) {
 			const key = sign(JSON.stringify(req.body), repo.secret)
 			if (key != secret) {
-				console.log('[auto] 校验失败')
+				log('校验失败')
 				res.send(false)
 				return false
 			}
 		}
 		else {
-			console.log('[auto] 未发现签名')
+			log('未发现签名')
 			res.send(false)
 			return false
 		}
 
-        console.log('[auto] 校验成功，正在拉取代码\n')
+        log('校验成功，正在拉取代码\n')
         const payload = req.body
         console.log(payload.repository.git_url)
 
         let pullResult = await execAsync('./src/exec/gitDownload.sh', [ repo.path ])
         if (!pullResult.state) {
-            console.log('[auto] 执行失败', pullResult.err)
+            log('执行失败, 错误信息如下')
+            console.log(pullResult.err)
             res.send(false)
             return false
         }
-        else {
-            console.log(pullResult.stdout)
-        }
-
+        
         console.log('[auto] 代码拉取完成，正在执行部署脚本\n')
         let execResult = await execAsync(path.join(repo.path, repo.deployScript))
 
         if (!execResult.state) {
 			// console.log('执行失败，请确保项目根目录中包含拥有可执行权限的 auto-deploy.sh 文件')
-            console.log('[auto] 执行失败，错误信息如下', execResult.err)
+            log('执行失败，错误信息如下')
+            console.log(execResult.err)
             res.send(false)
             return false
         }
 
-		console.log('[auto] 执行完成，待命中')
+		log('执行完成，待命中')
         res.send(true)
     })
 })
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export function log(msg) {
+    console.log(`\\033[42;30m AUTO \\033[40;32m ${msg}\\033[;0m`)
+}
