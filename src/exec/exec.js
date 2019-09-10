469d866638873419202dddf9abfb7beaf34e41ed
huang puguang <puguang_huang@fpi-inc.com>
2019-09-10 10:32:35 +0800

    fix: 无法执行提示

--- src/exec/exec.js
+++ src/exec/exec.js
@@ -1,32 +1,30 @@
 import { spawn } from 'child_process'
 import path from 'path'
+import { log } from '../utils'
 
 export function execAsync(filePath, args=[]) {
     return new Promise((resolve, reject) => {
-        const cmdProcess = spawn(filePath, args, {
-			cwd: path.isAbsolute(filePath) ? path.dirname(filePath) : undefined
-		})
-
-        cmdProcess.stdout.on('data', (data) => {
-            console.log(`${data}`)
-        })
-        
-        cmdProcess.stderr.on('data', (data) => {
-            console.log(`${data}`)
-        })
-        
-        cmdProcess.on('close', (code) => {
-            resolve({
-                state: true,
-                code
+        try {
+            const cmdProcess = spawn(filePath, args, {
+                cwd: path.isAbsolute(filePath) ? path.dirname(filePath) : undefined
             })
-        })
 
-        cmdProcess.on('error', (err) => {
-            resolve({
+            cmdProcess.on('error', err => resolve({
                 state: false,
                 err
-            })
-        })
+            }))
+    
+            cmdProcess.stdout.on('data', data => console.log(`${data}`))
+            
+            cmdProcess.stderr.on('data', data => console.log(`${data}`))
+            
+            cmdProcess.on('close', code => resolve({
+                state: true,
+                code
+            }))
+        }
+        catch (e) {
+            log(`无法执行 ${filePath}, 请检查脚本是否有执行权限`)
+        }
     })
 }
